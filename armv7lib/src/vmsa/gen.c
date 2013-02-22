8cf3f69256de1dceaa49eb52e3335ece435300e4
de7ec7ed <de7ec7ed@localhost.localdomain>
2013-02-22 01:02:05 -0500

    added GPL comments

--- armv7lib/src/vmsa/gen.c
+++ armv7lib/src/vmsa/gen.c
@@ -1,3 +1,23 @@
+/* This file is part of VERTIGO.
+ *
+ * (C) Copyright 2013, Siege Technologies <siegetechnologies.com>
+ *
+ * VERTIGO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VERTIGO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VERTIGO. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Written by Kirk Swidowski <kirk@swidowski.com>
+ */
+
 #include <config.h>
 #include <defines.h>
 #include <types.h>
